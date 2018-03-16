8c9f439640430cf667b2fc98d53c3b5769669a48
hostgjk <ccjguangzhou@163.com>
2018-03-16 17:15:15 +0800

    framework

--- RS.cpp
+++ RS.cpp
@@ -1,29 +1,52 @@
 #include "RS.h"
 
-RS::RS(){
+RS::RS()
+{
 	h = 1;
 	N = 9;
-	
-}
-void RS::Scatter(int round, int step, float* alleic_array){
 
 }
-void RS::Gather(int round, int step, float* alleic_array){
+void RS::Scatter(int rank, float*& alleic_array, int& array_len, int peer_array[])
+{
 
 }
-RS::RS(int rank){
-	h = 1;
+void RS::Gather(int rank, float*& alleic_array, int& array_len, int peer_array[])
+{
+
+}
+RS::RS(int rank)
+{
+	h = 2;
 	N = 9;
+	n = 3;
 	this->rank = rank;
-	
-}
-void RS::Sync(){
-	int k = 0;
-	for( k = 0; k<= h ; k++){
+	Utils::Val2Addr(rank, this->addr, h, n);
 
+
+}
+void RS::Sync(float g_array[], int W)
+{
+	int i = 0;
+	float* allelic_array = g_array;
+	int array_len = W;
+	for ( i = 0; i < h ; i++)
+	{
+		int rank = this->addr[i];
+		int peer_array[n];
+		Utils::GetPeers(i, this->addr, peer_array, h, n);
+		Scatter(rank, allelic_array, array_len, peer_array);
 	}
+	for (int i = h - 1; i >= 0 ; i--)
+	{
+		int rank = this->addr[i];
+		int peer_array[n];
+		Utils::GetPeers(i, this->addr, peer_array, h, n);
+		Gather(rank, allelic_array, array_len, peer_array);
+	}
+
 }
 
-RS::~RS(){
+RS::~RS()
+{
 
 }
--- RS.h
+++ RS.h
@@ -1,6 +1,6 @@
 #ifndef  _RS_H
 #define _RS_H
-
+#include "Utils.h"
 #include <stdio.h>
 #include <string.h>
 #include <vector>
@@ -13,22 +13,26 @@ class RS
 public:
 	RS();
 	RS(int rank);
-	void Scatter(int round, int step, float* alleic_array);
-	void Gather(int round, int step, float* alleic_array);
-	void Sync();
+	void Scatter(int rank, float*& alleic_array, int& array_len, int peer_array[]);
+	void Gather(int rank, float*& alleic_array, int& array_len, int peer_array[]);
+	void Sync(float g_array[], int W);
 	~RS();
 private:
 	int N;
 	int h;
-	string ipstr_0[SERVER_NUM] = {"127.0.0.1", "127.0.0.1","127.0.0.1", \
-	"127.0.0.1","127.0.0.1", "127.0.0.1","127.0.0.1","127.0.0.1", "127.0.0.1"};
-	string ipstr_1[SERVER_NUM] = {"127.0.0.1", "127.0.0.1","127.0.0.1", \
-	"127.0.0.1","127.0.0.1", "127.0.0.1","127.0.0.1","127.0.0.1", "127.0.0.1"};
-	int ports_0[SERVER_NUM]={4110,4111,4112,4113,4114,4115,4116,4117,4118};
-	int ports_1[SERVER_NUM]={4210,4211,4212,4213,4214,4215,4216,4217,4218};
+	int n;
+	string ipstr_0[SERVER_NUM] = {"127.0.0.1", "127.0.0.1", "127.0.0.1", \
+	                              "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"
+	                             };
+	string ipstr_1[SERVER_NUM] = {"127.0.0.1", "127.0.0.1", "127.0.0.1", \
+	                              "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"
+	                             };
+	int ports_0[SERVER_NUM] = {4110, 4111, 4112, 4113, 4114, 4115, 4116, 4117, 4118};
+	int ports_1[SERVER_NUM] = {4210, 4211, 4212, 4213, 4214, 4215, 4216, 4217, 4218};
 	int addr[MAX];
 	int rank;
-	
+
+
 };
 
-#endif 
+#endif
